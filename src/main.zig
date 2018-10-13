d778b936fcc9a660ae8f765b008c1818ba262360
Wink Saville <wink@saville.com>
2018-10-12 20:51:05 -0700

    Minimal NOP GLFW app.
    All this does is opens a window and then you can press escape or click on
    the close box to close the window and exit the program.
    
    This is a subset of GLFW
    [Getting started](https://www.glfw.org/docs/latest/quick.html).

--- src/main.zig
+++ src/main.zig
@@ -8,14 +8,51 @@ extern fn errorCallback(err: c_int, description: ?[*]const u8) void {
     os.abort();
 }
 
+extern fn windowCloseCallback(window: ?*gl.GLFWwindow) void {
+    warn("windwoCloseCallback called window={*}\n", window);
+}
+
+extern fn keyCallback(window: ?*gl.GLFWwindow, key: c_int,  scancode: c_int, action: c_int, mods: c_int) void {
+    warn("keyCallback: key={x} scancode={x} action={x} mods={x}\n", key, scancode, action, mods);
+    if ((key == gl.GLFW_KEY_ESCAPE) and (action == gl.GLFW_PRESS)) {
+        gl.glfwSetWindowShouldClose(window, gl.GLFW_TRUE);
+    }
+}
+
+pub fn errorExit(strg: []const u8) noreturn {
+    warn("{}", strg);
+    os.abort();
+}
+
 pub fn main() void {
     warn("main:+\n");
     defer warn("main:-\n");
 
+    // Ignore the previous callback funtion returned, we know it'll be null
     _ = gl.glfwSetErrorCallback(errorCallback);
+
     if (gl.glfwInit() == gl.GL_FALSE) {
-        warn("glfwInit failed\n");
-        os.abort();
+        errorExit("glfwInit failed\n");
     }
     defer gl.glfwTerminate();
+
+    var window = gl.glfwCreateWindow(640, 480, c"My Title", null, null) orelse {
+        errorExit("glfwCreateWindow failed\n");
+    };
+    defer gl.glfwDestroyWindow(window);
+
+    gl.glfwWindowHint(gl.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    gl.glfwWindowHint(gl.GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    // Ignore the previous callback funtion returned, we know it'll be null
+    _ = gl.glfwSetWindowCloseCallback(window, windowCloseCallback);
+
+    // Ignore the previous callback funtion returned, we know it'll be null
+    _ = gl.glfwSetKeyCallback(window, keyCallback);
+
+    gl.glfwMakeContextCurrent(window);
+
+    while (gl.glfwWindowShouldClose(window) == gl.GL_FALSE) {
+        gl.glfwPollEvents();
+    }
 }
