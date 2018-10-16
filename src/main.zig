b153766b9a5c7a81bcc65320f172e9168ac6045b
Wink Saville <wink@saville.com>
2018-10-16 14:09:33 -0700

    Use gl as the namespace for SDL2
    gl is Graphics Library and is shorter than using `sdl` or `sdl2` which is
    woudl be redundant anyway as all names are already prefixed with SDL_.
    This is why Marc Tiehuis just imported with `use`. But I like being more
    specific, although I may change my mind.

--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,36 @@
-use @import("../modules/zig-sdl2/src/index.zig");
+const gl = @import("../modules/zig-sdl2/src/index.zig");
 
 pub fn main() u8 {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-        SDL_Log(c"failed to initialized SDL\n");
+    if (gl.SDL_Init(gl.SDL_INIT_VIDEO | gl.SDL_INIT_AUDIO) != 0) {
+        gl.SDL_Log(c"failed to initialized SDL\n");
         return 1;
     }
-    defer SDL_Quit();
+    defer gl.SDL_Quit();
 
-    var renderer: *SDL_Renderer = undefined;
-    var window: *SDL_Window = undefined;
+    var renderer: *gl.SDL_Renderer = undefined;
+    var window: *gl.SDL_Window = undefined;
 
-    if (SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_SHOWN, &window, &renderer) != 0) {
-        SDL_Log(c"failed to initialize window and renderer\n");
+    if (gl.SDL_CreateWindowAndRenderer(640, 480, gl.SDL_WINDOW_SHOWN, &window, &renderer) != 0) {
+        gl.SDL_Log(c"failed to initialize window and renderer\n");
         return 1;
     }
-    defer SDL_DestroyRenderer(renderer);
-    defer SDL_DestroyWindow(window);
+    defer gl.SDL_DestroyRenderer(renderer);
+    defer gl.SDL_DestroyWindow(window);
 
-    SDL_SetWindowTitle(window, c"zig-sdl");
+    gl.SDL_SetWindowTitle(window, c"zig-sdl");
 
-    _ = SDL_SetRenderDrawColor(renderer, 0, 64, 128, 255);
-    _ = SDL_RenderClear(renderer);
+    _ = gl.SDL_SetRenderDrawColor(renderer, 0, 64, 128, 255);
+    _ = gl.SDL_RenderClear(renderer);
 
-    const r1 = SDL_Rect{ .x = 10, .y = 10, .w = 10, .h = 10 };
-    const r2 = SDL_Rect{ .x = 40, .y = 10, .w = 10, .h = 10 };
-    var rects = []SDL_Rect{ r1, r2 };
+    const r1 = gl.SDL_Rect{ .x = 10, .y = 10, .w = 10, .h = 10 };
+    const r2 = gl.SDL_Rect{ .x = 40, .y = 10, .w = 10, .h = 10 };
+    var rects = []gl.SDL_Rect{ r1, r2 };
 
-    _ = SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
-    _ = SDL_RenderFillRects(renderer, @ptrCast([*]SDL_Rect, &rects[0]), 2);
+    _ = gl.SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
+    _ = gl.SDL_RenderFillRects(renderer, @ptrCast([*]gl.SDL_Rect, &rects[0]), 2);
 
-    _ = SDL_RenderPresent(renderer);
+    _ = gl.SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    gl.SDL_Delay(3000);
     return 0;
 }
