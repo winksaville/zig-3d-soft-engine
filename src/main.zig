76bc86ea244053870523ef0f9e69fed44f366231
Wink Saville <wink@saville.com>
2018-10-23 10:19:49 -0700

    Change main.zig to use window.zig

--- src/main.zig
+++ src/main.zig
@@ -7,16 +7,17 @@ const warn = std.debug.warn;
 const gl = @import("../modules/zig-sdl2/src/index.zig");
 
 const ie = @import("input_events.zig");
+const wdw = @import("window.zig");
 
 const WindowState = struct.{
+    window: wdw.Window,
     quit: bool,
     leftMouseButtonDown: bool,
     ei: ie.EventInterface,
     bg_color: u32,
     fg_color: u32,
-    width: c_int,
-    height: c_int,
-    pixels: []u32,
+    width: usize,
+    height: usize,
 };
 
 fn handleKeyEvent(pThing: *c_void, event: *gl.SDL_Event) ie.EventResult {
@@ -51,7 +52,7 @@ fn handleMouseEvent(pThing: *c_void, event: *gl.SDL_Event) ie.EventResult {
             if (pWs.leftMouseButtonDown) {
                 var mouse_x: usize = @intCast(usize, event.motion.x);
                 var mouse_y: usize = @intCast(usize, event.motion.y);
-                pWs.pixels[(mouse_y * @intCast(usize, pWs.width)) + mouse_x] = pWs.fg_color;
+                pWs.window.putPixel(mouse_x, mouse_y, pWs.fg_color);
             }
         },
         else => {},
@@ -70,6 +71,7 @@ pub fn main() u8 {
     var pAllocator = &arena_allocator.allocator;
 
     var ws = WindowState.{
+        .window = undefined,
         .quit = false,
         .leftMouseButtonDown = false,
         .ei = ie.EventInterface.{
@@ -82,57 +84,13 @@ pub fn main() u8 {
         .fg_color = 0x00000000, // black
         .width = 640,
         .height = 480,
-        .pixels = pAllocator.alloc(u32, 640 * 480) catch |e| {
-            warn("Could not allocate pixels: {}\n", e);
-            return 1;
-        },
     };
 
-    // Initialize SDL
-    if (gl.SDL_Init(gl.SDL_INIT_VIDEO | gl.SDL_INIT_AUDIO) != 0) {
-        gl.SDL_Log(c"failed to initialized SDL\n");
-        return 1;
-    }
-    defer gl.SDL_Quit();
-
-    // Create Window
-    const x_pos: c_int = gl.SDL_WINDOWPOS_UNDEFINED;
-    const y_pos: c_int = gl.SDL_WINDOWPOS_UNDEFINED;
-    var window_flags: u32 = 0;
-    var window: *gl.SDL_Window = gl.SDL_CreateWindow(c"zig-3d-soft-engine", x_pos, y_pos, ws.width, ws.height, window_flags) orelse {
-        warn("Could not create Window error: {}\n", gl.SDL_GetError());
+    ws.window = wdw.Window.init(pAllocator, ws.width, ws.height, "zig-3d-soft-engine") catch |e| {
+        warn("Could not init window: {}\n", e);
         return 1;
     };
-    defer gl.SDL_DestroyWindow(window);
-
-    // This reduces CPU utilization but now dragging window is jerky
-    var r = gl.SDL_GL_SetSwapInterval(1);
-    if (r != 0) {
-        var b = gl.SDL_SetHint(gl.SDL_HINT_RENDER_VSYNC, c"1");
-        if (b != gl.SDL_bool.SDL_TRUE) {
-            warn("No VSYNC cpu utilization may be high!\n");
-        }
-    }
-
-    // Create Renderer
-    var renderer_flags: u32 = 0;
-    var renderer: *gl.SDL_Renderer = gl.SDL_CreateRenderer(window, -1, renderer_flags) orelse {
-        warn("Could not create Renderer error: {}\n", gl.SDL_GetError());
-        return 1;
-    };
-    defer gl.SDL_DestroyRenderer(renderer);
-
-    // Create Texture
-    var texture: *gl.SDL_Texture = gl.SDL_CreateTexture(renderer, gl.SDL_PIXELFORMAT_ARGB8888, gl.SDL_TEXTUREACCESS_STATIC, ws.width, ws.height) orelse {
-        warn("Could not create Texture error: {}\n", gl.SDL_GetError());
-        return 1;
-    };
-    defer gl.SDL_DestroyTexture(texture);
-
-    // Create Pixel buffer
-    for (ws.pixels) |*pixel| {
-        pixel.* = ws.bg_color;
-    }
+    defer ws.window.deinit();
 
     while (!ws.quit) {
         // Process all events
@@ -150,10 +108,7 @@ pub fn main() u8 {
         }
 
         // Update display
-        _ = gl.SDL_UpdateTexture(texture, null, @ptrCast(*const c_void, &ws.pixels[0]), ws.width * @sizeOf(u32));
-        _ = gl.SDL_RenderClear(renderer);
-        _ = gl.SDL_RenderCopy(renderer, texture, null, null);
-        _ = gl.SDL_RenderPresent(renderer);
+        ws.window.present();
     }
 
     return 0;
